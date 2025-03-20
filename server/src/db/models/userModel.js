3038bfd3fc09fb3d266c846ef7dd6956390c11a5
Ahmed S. <s.ahmed.profiquence@gmail.com>
2025-03-20 16:28:38 +0530

    user model updated for password handling

--- server/src/db/models/userModel.js
+++ server/src/db/models/userModel.js
@@ -3,6 +3,12 @@ import mongoose, { Schema } from "mongoose";
 const UserSchema = new Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  startupAssociated: {
+    type: Schema.Types.ObjectId,
+    ref: "startup",
+    required: true,
+  },
 });
 
 const User = mongoose.models.user || mongoose.model("user", UserSchema);
