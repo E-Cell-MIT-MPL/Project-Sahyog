a8d95f74b596f5b5ac028e2ceb6c39269b34084e
Ahmed S. <s.ahmed.profiquence@gmail.com>
2025-03-20 15:28:16 +0530

    Created Mongoose model for user

---
+++ server/src/db/models/userModel.js
@@ -0,0 +1,10 @@
+import mongoose, { Schema } from "mongoose";
+
+const UserSchema = new Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+});
+
+const User = mongoose.models.user || mongoose.model("user", UserSchema);
+
+export default User;
